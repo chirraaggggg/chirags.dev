cbb3965a6bc9877176652789392bbb028e54871f
Chirag <chhirag920@gmail.com>
2026-05-03 13:37:20 +0530

    remove .next from tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+J7/eDZWOi2wjaV2yGMmurpQ+s8YDn+u6JIBiUOhEqM=\"\n}"
--- .next/dev/static/chunks/src_app_(app)_page_tsx_6c86c159._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_82cc592b._.js",
-  "static/chunks/node_modules_f08c5601._.js"
-],
-    source: "dynamic"
-});
